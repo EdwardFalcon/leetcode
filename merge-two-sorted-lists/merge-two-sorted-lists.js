c9ba683c7fef92e05fa3c060c4e5da1d1185f6f5
Edward Falcon <devedwardfalcon@gmail.com>
2020-06-21 14:11:01 +0300

    add merge two sorted lists

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {        
+  if (!l1 || !l2) return l1 || l2;    
+  if (l1.val < l2.val) return new ListNode(l1.val, mergeTwoLists(l1.next, l2));    
+  return new ListNode(l2.val, mergeTwoLists(l1, l2.next));    
+};
