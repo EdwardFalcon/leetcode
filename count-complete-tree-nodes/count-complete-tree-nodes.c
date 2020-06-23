4571f2a59c267c19dcd0160c91a439ba0ca95806
Edward Falcon <devedwardfalcon@gmail.com>
2020-06-23 11:38:58 +0300

    add count complete tree nodes

---
+++ count-complete-tree-nodes/count-complete-tree-nodes.c
@@ -0,0 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int countNodes(struct TreeNode* root){
+    return root ? 1 + countNodes(root->left) + countNodes(root->right) : 0;
+}
