7f2185ca1f733060a6d46906dc890d3419beac05
EdwardFalcon <devedwardfalcon@gmail.com>
2022-02-03 10:08:53 +0300

    add min cost climbing stairs

---
+++ easy/min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  const dp = [cost[0], cost[1]];
+  for (let i = 2; i < cost.length; i++) {
+    dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+  }
+  return Math.min(dp[dp.length - 1], dp[dp.length - 2]);
+};
