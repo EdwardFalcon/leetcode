383a61c3a878f39175e12eaa397a7715c18e6df5
EdwardFalcon <devedwardfalcon@gmail.com>
2022-02-10 12:34:53 +0300

    add longest common prefix

---
+++ easy/longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 1) {
+    return strs[0];
+  }
+  let prefix = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== strs[0][i]) {
+        return prefix;
+      }
+    }
+    prefix += strs[0][i];
+  }
+  return prefix;
+};
