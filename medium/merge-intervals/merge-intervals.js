26c2661c774db9135ebbf5917eb2931787c2da1a
EdwardFalcon <devedwardfalcon@gmail.com>
2022-02-15 09:43:21 +0300

    add merge intervals

---
+++ medium/merge-intervals/merge-intervals.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const result = [intervals[0]];
+  for (let i = 1; i < intervals.length; i++) {
+    const last = result[result.length - 1];
+    const curr = intervals[i];
+    if (last[1] < curr[0]) {
+      result.push(curr);
+      continue;
+    }
+    last[0] = Math.min(last[0], curr[0]);
+    if (last[1] <= curr[1]) {
+      last[1] = Math.max(last[1], curr[1]);
+    }
+  }
+  return result;
+};
