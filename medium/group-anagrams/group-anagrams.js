9bc8e48486140f74902f04d3e30399c40b31ac13
EdwardFalcon <devedwardfalcon@gmail.com>
2022-02-16 08:59:52 +0300

    add group anagrams

---
+++ medium/group-anagrams/group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = {};
+  strs.forEach((str) => {
+    const key = str.split('').sort().join('');
+    if (map[key]) {
+      map[key].push(str);
+    } else {
+      map[key] = [str];
+    }
+  });
+  return Object.values(map);
+};
