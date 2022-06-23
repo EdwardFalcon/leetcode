586acb2fcd86745f12d4d21218c4f48b33e4cb8f
EdwardFalcon <devedwardfalcon@gmail.com>
2022-06-23 14:45:19 +0300

    add range sum query immutable

---
+++ easy/range-sum-query-immutable/range-sum-query-immutable.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.sums = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    this.sums.push(this.sums[i - 1] + nums[i]);
+  }
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  if (left === 0) {
+    return this.sums[right];
+  }
+  return this.sums[right] - this.sums[left - 1];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
