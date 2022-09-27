e84b5ecc1bd50df6a79c4a0d9f2eb791a6ef1127
EdwardFalcon <devedwardfalcon@gmail.com>
2022-09-27 14:11:54 +0300

    fix jump game

--- medium/jump-game/jump-game.js
+++ medium/jump-game/jump-game.js
@@ -4,13 +4,10 @@
  */
 var canJump = function (nums) {
   let jump = nums.length - 1;
-  let steps = 0;
   for (let i = nums.length - 1; i >= 0; i--) {
     if (i + nums[i] >= jump) {
       jump = i;
-      steps++;
     }
   }
-  console.log(steps);
   return jump === 0;
 };
