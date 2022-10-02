024040955b971e9c51290e02657e44ab3981e6a7
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-02 18:58:44 +0300

    add subarray product less than k

---
+++ medium/subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function (nums, k) {
+  if (k <= 1) {
+    return 0;
+  }
+  let result = 0;
+  let left = 0;
+  let right = 0;
+  let product = 1;
+  while (right < nums.length) {
+    product *= nums[right];
+    while (product >= k) {
+      product /= nums[left++];
+    }
+    result += right - left + 1;
+    right++;
+  }
+  return result;
+};
