3d926a6cc474fb683f95943184fc0a32a93da64a
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-14 00:45:49 +0300

    add edit distance

---
+++ hard/edit-distance/edit-distance.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  const dp = Array(word1.length + 1)
+    .fill(0)
+    .map((a) => Array(word2.length + 1));
+  for (let i = 0; i <= word1.length; i++) {
+    for (let j = 0; j <= word2.length; j++) {
+      if (i === 0) {
+        dp[i][j] = j;
+      } else if (j === 0) {
+        dp[i][j] = i;
+      } else if (word1[i - 1] == word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i][j - 1], dp[i - 1][j - 1], dp[i - 1][j]);
+      }
+    }
+  }
+  return dp[word1.length][word2.length];
+};
