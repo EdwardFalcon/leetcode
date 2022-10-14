d4277c725002e389df2366c586539e4d7672b9a2
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-14 18:39:09 +0300

    add daily temperatures

---
+++ medium/daily-temperatures/daily-temperatures.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} T
+ * @return {number[]}
+ */
+var dailyTemperatures = function (T) {
+  const result = Array(T.length).fill(0);
+  const stack = [];
+  for (let i = 0; i < T.length; i++) {
+    while (stack.length && T[stack[stack.length - 1]] < T[i]) {
+      const j = stack.pop();
+      result[j] = i - j;
+    }
+    stack.push(i);
+  }
+  return result;
+};
