6ddef962be0d447cb14d6065cdee9101e4d06aa2
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-14 12:40:54 +0300

    add build array from permutation

---
+++ easy/build-array-from-permutation/build-array-from-permutation.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function (nums) {
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    nums[i] = nums[i] + n * (nums[nums[i]] % n);
+  }
+  for (let i = 0; i < n; i++) {
+    nums[i] = Math.floor(nums[i] / n);
+  }
+  return nums;
+};
