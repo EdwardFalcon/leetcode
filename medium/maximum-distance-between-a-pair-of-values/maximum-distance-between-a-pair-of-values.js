d84d343619423e0604eae05444033e7538ac9558
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-23 20:15:03 +0300

    add maximum distance between a pair of values

---
+++ medium/maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function (nums1, nums2) {
+  let result = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    let left = i;
+    let right = nums2.length - 1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (nums2[mid] < nums1[i]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+      if (right !== i - 1) {
+        result = Math.max(result, right - i);
+      }
+    }
+  }
+
+  return result;
+};
