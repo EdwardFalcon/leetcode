abd6e70699257e489fe9e31c1e9a8effab196441
EdwardFalcon <devedwardfalcon@gmail.com>
2022-10-25 19:58:02 +0300

    add n queens

---
+++ hard/n-queens/n-queens.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+  const board = Array(n)
+    .fill()
+    .map(() => Array(n).fill('.'));
+  const result = [];
+  dfs(board, 0, result);
+  return result;
+};
+
+var check = function (board, row, col) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][col] === 'Q') {
+      return false;
+    }
+    if (row - i >= 0 && col - i >= 0 && board[row - i][col - i] == 'Q') {
+      return false;
+    }
+    if (
+      row - i >= 0 &&
+      col + i < board.length &&
+      board[row - i][col + i] == 'Q'
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+var dfs = function (board, row, result) {
+  if (row === board.length) {
+    result.push([...board].map((row) => row.join('')));
+    return;
+  }
+  for (let col = 0; col < board.length; col++) {
+    if (check(board, row, col)) {
+      board[row][col] = 'Q';
+      dfs(board, row + 1, result);
+      board[row][col] = '.';
+    }
+  }
+};
