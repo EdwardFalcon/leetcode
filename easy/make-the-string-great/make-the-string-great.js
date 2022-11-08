759c5cce1b76a39143fc06efda888a529c4e06dd
EdwardFalcon <devedwardfalcon@gmail.com>
2022-11-08 16:14:29 +0300

    add make the string great

---
+++ easy/make-the-string-great/make-the-string-great.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function (s) {
+  const stack = [];
+  let pos = 0;
+  while (pos < s.length) {
+    const char = s[pos];
+    if (
+      stack.length &&
+      Math.abs(stack[stack.length - 1].charCodeAt(0) - char.charCodeAt(0)) ===
+        32
+    ) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+    pos++;
+  }
+  return stack.join('');
+};
